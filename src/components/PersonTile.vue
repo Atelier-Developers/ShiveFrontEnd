0355e14a6279f3575db66e608e60537a363cc5a5
tavakkoliamirmohammad <tavakkoli.amirmohammad@gmail.com>
2020-09-22 23:50:37 +0330

    fixed person tile

--- src/components/PersonTile.vue
+++ src/components/PersonTile.vue
@@ -4,6 +4,24 @@
       <v-list-item-title>{{ person.name }}</v-list-item-title>
       <v-list-item-subtitle>{{ person.student_no }}</v-list-item-subtitle>
     </v-list-item-content>
+    <v-list-item-action>
+     <v-btn icon>
+       <v-icon
+           color="green"
+       >
+         check
+       </v-icon>
+     </v-btn>
+    </v-list-item-action>
+    <v-list-item-action>
+      <v-btn icon>
+        <v-icon
+            color="red"
+        >
+          close
+        </v-icon>
+      </v-btn>
+    </v-list-item-action>
   </v-list-item>
 </template>
 
