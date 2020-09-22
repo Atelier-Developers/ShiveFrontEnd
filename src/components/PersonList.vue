fab7e85c883a04cffff26f143d511e8d54f16c4b
tavakkoliamirmohammad <tavakkoli.amirmohammad@gmail.com>
2020-09-22 23:16:56 +0330

    add person tile and list

---
+++ src/components/PersonList.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card tile>
+    <PersonTile v-for="person in persons" :person="person" :key="person.pk"/>
+  </v-card>
+</template>
+
+<script>
+import PersonTile from "@/components/PersonTile";
+
+export default {
+  name: "PersonList",
+  components: {PersonTile},
+  props: ['persons']
+}
+</script>
+
+<style scoped>
+
+</style>
