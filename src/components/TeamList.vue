e5dfc03b2a9ad78c5c9f5ea4b726ac285bfb5f05
tavakkoliamirmohammad <tavakkoli.amirmohammad@gmail.com>
2020-09-23 20:13:43 +0330

    add team list delete dialog

--- src/components/TeamList.vue
+++ src/components/TeamList.vue
@@ -17,12 +17,28 @@
       >
         <v-icon>create</v-icon>
       </v-btn>
-      <v-btn
-          color="error"
-          icon
-      >
-        <v-icon>delete</v-icon>
-      </v-btn>
+      <v-dialog v-model="dialog" persistent max-width="290">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+              color="error"
+              icon
+              v-bind="attrs"
+              v-on="on"
+          >
+            <v-icon>delete</v-icon>
+          </v-btn>
+        </template>
+        <v-card>
+          <v-card-title class="headline">حذف تیم</v-card-title>
+          <v-card-text>آیا از حذف این تیم مطمئن هستید؟
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary darken-1" text @click="dialog = false">انصراف</v-btn>
+            <v-btn color="error darken-1" text @click="dialog = false">حذف</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-card-actions>
   </v-card>
 </template>
@@ -34,6 +50,11 @@ export default {
   name: "TeamList",
   components: {TeamPersonTile},
   props: ['persons', 'title'],
+  data() {
+    return {
+      dialog: false,
+    }
+  }
 }
 </script>
 
