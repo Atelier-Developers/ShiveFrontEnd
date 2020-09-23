26e74344dae18d6f2cf7c44f8bbd1193e1188491
arefsa <arefsayareh@gmail.com>
2020-09-23 23:33:38 +0330

    add present page

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from "@/views/Login";
 import Signup from "@/views/Signup";
 import PersonManagement from "@/views/PersonManagment";
 import TeamManagement from "@/views/TeamManagement";
+import PresentPage from "../views/PresentPage";
 
 Vue.use(VueRouter)
 
@@ -33,6 +34,11 @@ const routes = [
         path: '/team-management',
         name: 'TeamManagement',
         component: TeamManagement
+    },
+    {
+        path: '/present-page',
+        name: 'PresentPage',
+        component: PresentPage
     }
 ]
 
