4ea3fa3e0fdf02cf817551aacca050dff14a75bd
tavakkoliamirmohammad <tavakkoli.amirmohammad@gmail.com>
2020-09-25 17:54:06 +0330

    removed some intial pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import Login from "@/views/Login";
 import Signup from "@/views/Signup";
 import PersonManagement from "@/views/PersonManagment";
@@ -12,11 +11,6 @@ import SubjectManagement from "../views/SubjectManagement";
 Vue.use(VueRouter)
 
 const routes = [
-    {
-        path: '/',
-        name: 'Home',
-        component: Home
-    },
     {
         path: '/login',
         name: 'Login',
