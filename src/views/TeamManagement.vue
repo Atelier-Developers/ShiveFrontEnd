c51bee095dc280a2edda5f061864cd27f89d3dce
arefsa <arefsayareh@gmail.com>
2020-09-26 00:28:49 +0330

    i fuck my self very hard

--- src/views/TeamManagement.vue
+++ src/views/TeamManagement.vue
@@ -1,12 +1,23 @@
 <template>
-  <v-container>
-    <v-row v-if="teams.length !== 0" class="fill-height">
-      <v-col cols="12" class="col-md-4" v-for="team in teams">
-        <TeamList :team="team"/>
-      </v-col>
-    </v-row>
-  </v-container>
-
+  <div class="mx-3">
+    <v-container>
+      <v-row v-if="teams.length !== 0" class="fill-height">
+        <v-col cols="12" class="col-md-4" v-for="team in teams">
+          <TeamList :team="team"/>
+        </v-col>
+      </v-row>
+    </v-container>
+    <div id="floating-button" class="mb-10">
+      <v-btn
+          fab
+          @click="sendNewStateToServer"
+          :loading="loading"
+          color="accent"
+      >
+        <v-icon>add</v-icon>
+      </v-btn>
+    </div>
+  </div>
 </template>
 
 <script>
