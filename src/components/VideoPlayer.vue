e6d4064cd447a9496761b1f74b86cc03dd523039
arefsa <arefsayareh@gmail.com>
2020-09-26 18:35:55 +0330

    FOCUUUUUUUUS NOT WORKING >>>>:((((((((((((((((((

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="video-player" v-on:click="pause_play_click">
+  <div
+      class="video-player"
+      v-on:mousemove="mouse_move"
+      v-on:mouseup="video_player_mouse_up"
+      v-on:keydown="video_player_key_press"
+      v-on:click="video_player_click"
+      autofocus
+  >
     <video
+        v-on:click="pause_play_click"
         class="video"
         id="video-player"
         v-on:timeupdate="video_time_update"
@@ -14,9 +22,9 @@
           <div class="slash">/</div>
           <div class="length">00:00</div>
         </div>
-        <div class="bar">
+        <div class="bar" v-on:click="click_bar">
           <div class="complete"></div>
-          <div class="handler">
+          <div class="handler" v-on:mousedown="handler_mouse_down">
             <div class="handler-center"></div>
           </div>
         </div>
@@ -40,16 +48,17 @@ export default class VideoPlayer extends Vue {
   video_elem = null;
 
   play_pause_toggle = true;
+  holding_progress_handler = false;
+  new_seek_time = -1;
   progress_elems = null;
   time = 0;
+
 
   get player() {
     if (this.video_elem)
       return this.video_elem;
     else {
       this.video_elem = document.getElementById('video-player');
-      console.log(this.video_elem);
-      console.log(this.video_elem.duration);
       this.progress.length.innerHTML = parseInt(this.video_elem.duration);
     }
     return this.video_elem;
@@ -86,6 +95,54 @@ export default class VideoPlayer extends Vue {
     this.progress.complete.style.width = percentage + '%';
     this.progress.handler.style.left = percentage + '%';
     this.progress.time_pass.innerHTML = parseInt(this.time);
+  }
+
+  handler_mouse_down() {
+    this.holding_progress_handler = true;
+  }
+
+  video_player_mouse_up() {
+    if (this.holding_progress_handler) {
+      this.holding_progress_handler = false;
+      this.new_seek_time = this.new_seek_time / this.progress.handler.parentElement.offsetWidth * this.video_length;
+      this.player.currentTime = this.new_seek_time;
+    }
+  }
+
+  mouse_move(ev) {
+    if (this.holding_progress_handler) {
+      let left_offset = this.find_left_offset(this.progress.complete);
+      let left = ev.clientX - left_offset;
+      if (0 <= left && left <= this.progress.handler.parentElement.offsetWidth)
+        this.progress.handler.style.left = left + 'px';
+      this.new_seek_time = left;
+    }
+  }
+
+  video_player_key_press(ev) {
+    console.log(ev);
+  }
+
+  video_player_click(ev) {
+    setTimeout(() => {
+      document.getElementsByClassName('video-player')[0].focus();
+    },10);
+  }
+
+  find_left_offset(elem) {
+    let offset = 0
+    while (elem) {
+      offset += elem.offsetLeft;
+      elem = elem.parentElement;
+    }
+    return offset;
+  }
+
+  click_bar(ev) {
+    let left_offset = this.find_left_offset(this.progress.handler.parentElement);
+    let left = ev.clientX - left_offset;
+    this.new_seek_time = left / this.progress.handler.parentElement.offsetWidth * this.video_length;
+    this.player.currentTime = this.new_seek_time;
   }
 }
 </script>
@@ -130,18 +187,27 @@ export default class VideoPlayer extends Vue {
   transform: translate(-50%, -50%);
 }
 
-.video-player{
+.video-player {
   height: 400px;
   position: relative;
   width: fit-content;
   background-color: black;
 }
 
-.video-player #video-player{
+.video-player:focus {
+  box-shadow: 0 0 20px black !important;
+}
+
+.video-player {
+  user-select: none;
+}
+
+.video-player #video-player {
   position: relative;
   height: 100%;
 }
-.video-player .progress-bar{
+
+.video-player .progress-bar {
   position: absolute;
   bottom: 0px;
   right: 0px;
@@ -150,10 +216,11 @@ export default class VideoPlayer extends Vue {
   align-items: center;
   width: 100%;
   padding: 0 15px 12px;
-  background-image: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0.2) 42%, rgba(0,0,0,0.7)) ;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.2) 42%, rgba(0, 0, 0, 0.7));
   color: white;
 }
-.video-player .progress-bar .time{
+
+.video-player .progress-bar .time {
   display: flex;
   flex-direction: row-reverse;
 }
