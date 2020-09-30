15114cf199f338cccf0f50928421aaab3ea5317a
tavakkoliamirmohammad <tavakkoli.amirmohammad@gmail.com>
2020-09-30 17:26:07 +0330

    add dialog loading and fill for description dialog

--- src/views/TeamPresentPage.vue
+++ src/views/TeamPresentPage.vue
@@ -1,190 +1,199 @@
 <template>
-    <div v-if="!pageLoading">
-        <template v-if="teamPresentation !== undefined">
-            <v-container>
-                <v-row justify="center">
-                    <h1>{{teamPresentation.subject.title}}</h1>
-                </v-row>
-            </v-container>
-            <v-container>
-                <PresentationFileComponent :loading="pageLoading" class="mx-5 mx-sm-15" :is-deletable="true" :delete-file="deleteFile"
-                                           :files="teamPresentation.files"/>
-            </v-container>
-            <v-container>
-                <v-card class="mx-5 mx-sm-15" tile :loading="pageLoading">
-                    <v-card-title class="pr-10">
-                        <v-icon right>description</v-icon>
-                        توضیحات
-                    </v-card-title>
-                    <v-divider/>
-                    <v-card-text v-if="teamPresentation.description">
-                   <span style="white-space: pre">
-                    {{teamPresentation.description}}
-                   </span>
-                    </v-card-text>
-                    <v-card-text class="text-center" v-else>
-                        توضیحی وارد نشده است...
-                    </v-card-text>
-                    <v-card-actions class="justify-end">
-                        <v-btn
-                                class="ml-5 mb-2"
-                                color="primary"
-                                rounded
-                                @click.stop="descriptionEditDialog = true"
-                        >
-                            <v-icon color="accent">create</v-icon>
-                        </v-btn>
-
-                        <v-dialog
-                                v-model="descriptionEditDialog"
-                                max-width="570"
-                        >
-                            <v-card>
-                                <v-card-title>
-                                    <span class="headline">توضیحات جدید</span>
-                                </v-card-title>
-                                <v-card-text>
-                                    <v-textarea outlined auto-grow rows="3" label="توضیحات خود را وارد کنید..."
-                                                v-model="item.description"/>
-                                </v-card-text>
-                                <v-card-actions>
-                                    <v-spacer></v-spacer>
-                                    <v-btn color="blue darken-1" text @click="descriptionEditDialog = false">انصراف</v-btn>
-                                    <v-btn color="blue darken-1" text @click="() => editDescription(item.description)">
-                                        تایید
-                                    </v-btn>
-                                </v-card-actions>
-                            </v-card>
-                        </v-dialog>
-                    </v-card-actions>
-                </v-card>
-            </v-container>
-
-            <v-container>
-                <PresentationComment :comments="teamPresentation.comments" class="mx-5 mx-sm-15"/>
-            </v-container>
-            <div id="floating-button" class="mb-10 mr-3">
-                <v-btn
-                        fab
-                        @click="fileUploadDialog = true"
-                        color="primary"
-                >
-                    <v-icon color="accent">add</v-icon>
-                </v-btn>
-                <v-dialog
-                        v-model="fileUploadDialog"
-                        max-width="570"
-                >
-                    <v-card :loading="fileUploadLoading" :disabled="fileUploadLoading">
-                        <v-card-title>
-                            <span class="headline">آپلود فایل جدید</span>
-                        </v-card-title>
-                        <v-card-text>
-                            <v-row>
-                                <v-col cols="12">
-                                    <v-text-field label="نام فایل" v-model="item.name" outlined dense/>
-                                </v-col>
-                                <v-col cols="12">
-                                    <v-file-input label="فایل جدید خود را انتخاب کنید" v-model="item.file" outlined dense/>
-                                </v-col>
-                            </v-row>
-                        </v-card-text>
-                        <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn color="blue darken-1" text @click="fileUploadDialog = false">انصراف</v-btn>
-                            <v-btn color="blue darken-1" text @click="handleFileUpload ">تایید</v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-dialog>
-            </div>
-        </template>
-        <v-row v-else justify="center" align="center" class="fill-height">
-            <div>
-                <v-row>
-                    <v-col cols="12" class="text-center">
-                        <v-icon size="100">group</v-icon>
-                    </v-col>
-
-                    <v-col cols="12" class="text-center display-1">
-                        هنوز تیم شما تعیین نشده است!
-                    </v-col>
-                </v-row>
-            </div>
+  <div v-if="!pageLoading">
+    <template v-if="teamPresentation !== undefined">
+      <v-container>
+        <v-row justify="center">
+          <h1>{{ teamPresentation.subject.title }}</h1>
         </v-row>
-    </div>
-    <Spinner v-else />
+      </v-container>
+      <v-container>
+        <PresentationFileComponent :loading="pageLoading" class="mx-5 mx-sm-15" :is-deletable="true"
+                                   :delete-file="deleteFile"
+                                   :files="teamPresentation.files"/>
+      </v-container>
+      <v-container>
+        <v-card class="mx-5 mx-sm-15" tile :loading="pageLoading">
+          <v-card-title class="pr-10">
+            <v-icon right>description</v-icon>
+            توضیحات
+          </v-card-title>
+          <v-divider/>
+          <v-card-text v-if="teamPresentation.description">
+                   <span style="white-space: pre">
+                    {{ teamPresentation.description }}
+                   </span>
+          </v-card-text>
+          <v-card-text class="text-center" v-else>
+            توضیحی وارد نشده است...
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn
+                class="ml-5 mb-2"
+                color="primary"
+                rounded
+                @click.stop="openDescriptionEditDialog"
+            >
+              <v-icon color="accent">create</v-icon>
+            </v-btn>
+
+            <v-dialog
+                v-model="descriptionEditDialog"
+                max-width="570"
+            >
+              <v-card :loading="descriptionLoading" :disabled="descriptionLoading">
+                <v-card-title>
+                  <span class="headline">توضیحات جدید</span>
+                </v-card-title>
+                <v-card-text>
+                  <v-textarea outlined auto-grow rows="3" label="توضیحات خود را وارد کنید..."
+                              v-model="item.description"/>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" text @click="descriptionEditDialog = false">انصراف</v-btn>
+                  <v-btn color="blue darken-1" text @click="() => editDescription(item.description)">
+                    تایید
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+
+      <v-container>
+        <PresentationComment :comments="teamPresentation.comments" class="mx-5 mx-sm-15"/>
+      </v-container>
+      <div id="floating-button" class="mb-10 mr-3">
+        <v-btn
+            fab
+            @click="fileUploadDialog = true"
+            color="primary"
+        >
+          <v-icon color="accent">add</v-icon>
+        </v-btn>
+        <v-dialog
+            v-model="fileUploadDialog"
+            max-width="570"
+        >
+          <v-card :loading="fileUploadLoading" :disabled="fileUploadLoading">
+            <v-card-title>
+              <span class="headline">آپلود فایل جدید</span>
+            </v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field label="نام فایل" v-model="item.name" outlined dense/>
+                </v-col>
+                <v-col cols="12">
+                  <v-file-input label="فایل جدید خود را انتخاب کنید" v-model="item.file" outlined dense/>
+                </v-col>
+              </v-row>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="fileUploadDialog = false">انصراف</v-btn>
+              <v-btn color="blue darken-1" text @click="handleFileUpload ">تایید</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </div>
+    </template>
+    <v-row v-else justify="center" align="center" class="fill-height">
+      <div>
+        <v-row>
+          <v-col cols="12" class="text-center">
+            <v-icon size="100">group</v-icon>
+          </v-col>
+
+          <v-col cols="12" class="text-center display-1">
+            هنوز تیم شما تعیین نشده است!
+          </v-col>
+        </v-row>
+      </div>
+    </v-row>
+  </div>
+  <Spinner v-else/>
 
 </template>
 
 <script>
-    import FileTile from "../components/FileTile";
-    import {mapActions, mapGetters} from "vuex";
-    import PresentationFileComponent from "../components/PresentationFileComponent";
-    import PresentationComment from "@/components/PresentationComment";
-    import Spinner from "../components/Spinner";
+import FileTile from "../components/FileTile";
+import {mapActions, mapGetters} from "vuex";
+import PresentationFileComponent from "../components/PresentationFileComponent";
+import PresentationComment from "@/components/PresentationComment";
+import Spinner from "../components/Spinner";
 
-    export default {
-        name: "TeamPresentPage",
-        components: {Spinner, PresentationComment, PresentationFileComponent, FileTile},
-        data() {
-            return {
-                item: {
-                    description: '',
-                    file: '',
-                    name: ''
-                },
-                descriptionEditDialog: false,
-                fileUploadDialog: false,
-                fileUploadLoading: false,
-                pageLoading: true,
-            }
-        },
-        computed: {
-            ...mapGetters('presentationModule', ['teamPresentation']),
-        },
-        methods: {
-            ...mapActions('presentationModule', ['getTeamPresentation', 'uploadFileForTeamPresentation', 'setDescriptionForTeamPresentation', 'deleteFileFromTeamPresentation']),
-            handleFileUpload() {
-                this.fileUploadLoading = true;
-                this.uploadFile(this.item.file, this.item.name).then(() => {
-                    this.fileUploadLoading = false;
-                    this.fileUploadDialog = false;
-                })
-            },
-            uploadFile(file, name) {
-                return this.uploadFileForTeamPresentation({
-                    pk: this.teamPresentation.pk,
-                    file: file,
-                    name: name
-                }).then(() => {
-                    return this.getTeamPresentation();
-                }).then(() => {
-                    this.item.file = '';
-                });
-            },
-            editDescription(description) {
-                this.descriptionEditDialog = false;
-                this.setDescriptionForTeamPresentation({
-                    pk: this.teamPresentation.pk,
-                    description: {description: description}
-                }).then(() => {
-                    this.item.description = '';
-                    this.getTeamPresentation();
-                });
-            },
-            deleteFile(id) {
-                return this.deleteFileFromTeamPresentation(id).then(() => {
-                    return this.getTeamPresentation();
-                });
-            }
-        },
-        mounted() {
-            this.pageLoading = true;
-            this.getTeamPresentation().finally(() => {
-                this.pageLoading = false;
-            });
-        }
+export default {
+  name: "TeamPresentPage",
+  components: {Spinner, PresentationComment, PresentationFileComponent, FileTile},
+  data() {
+    return {
+      item: {
+        description: '',
+        file: '',
+        name: ''
+      },
+      descriptionLoading: false,
+      descriptionEditDialog: false,
+      fileUploadDialog: false,
+      fileUploadLoading: false,
+      pageLoading: true,
     }
+  },
+  computed: {
+    ...mapGetters('presentationModule', ['teamPresentation']),
+  },
+  methods: {
+    ...mapActions('presentationModule', ['getTeamPresentation', 'uploadFileForTeamPresentation', 'setDescriptionForTeamPresentation', 'deleteFileFromTeamPresentation']),
+    handleFileUpload() {
+      this.fileUploadLoading = true;
+      this.uploadFile(this.item.file, this.item.name).then(() => {
+        this.fileUploadLoading = false;
+        this.fileUploadDialog = false;
+      })
+    },
+    openDescriptionEditDialog() {
+      this.item.description = this.teamPresentation.description
+      this.descriptionEditDialog = true
+    },
+    uploadFile(file, name) {
+      return this.uploadFileForTeamPresentation({
+        pk: this.teamPresentation.pk,
+        file: file,
+        name: name
+      }).then(() => {
+        return this.getTeamPresentation();
+      }).then(() => {
+        this.item.file = '';
+      });
+    },
+    editDescription(description) {
+      this.descriptionLoading = true;
+      this.setDescriptionForTeamPresentation({
+        pk: this.teamPresentation.pk,
+        description: {description: description}
+      }).then(() => {
+        return this.getTeamPresentation();
+      }).then(() => {
+        this.item.description = '';
+        this.descriptionLoading = false;
+        this.descriptionEditDialog = false;
+      });
+    },
+    deleteFile(id) {
+      return this.deleteFileFromTeamPresentation(id).then(() => {
+        return this.getTeamPresentation();
+      });
+    }
+  },
+  mounted() {
+    this.pageLoading = true;
+    this.getTeamPresentation().finally(() => {
+      this.pageLoading = false;
+    });
+  }
+}
 </script>
 
 <style scoped>
